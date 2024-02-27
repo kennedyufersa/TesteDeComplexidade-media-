dfd6846cd884eb1b141b9ab3551606351e57dc7e
Bazch <mikael.vidal@gmail.com>
2024-02-27 14:44:53 -0300

    Função para escrever tempos de teste em arquivo txt

--- TesteDeComplexidade.c
+++ TesteDeComplexidade.c
@@ -5,18 +5,16 @@
 
 double *complexidadeMedia(void(func)(int *, int), int qntdVezesTestada);
 void insertionSort(int *arr, int n);
-char loading( int i);
+char loading(int i);
 char *setPorcentagem(int qntdTestes, int total, int tamanhoBarra);
+void escreverEmArquivo(double *tempo, int qntdDeTestes);
 
 int main()
 {
     int qntdTestes = 5;
     double *tempos = complexidadeMedia(insertionSort, qntdTestes);
-    int i;
-    for (i = 0; i < qntdTestes; i++)
-    {
-        printf("\nTempo [%i]: %f\n", i + 1, tempos[i]);
-    }
+    
+    escreverEmArquivo(tempos,qntdTestes);
 
     free(tempos);
 }
@@ -45,14 +43,16 @@ double *complexidadeMedia(void(func)(int *, int), int qntdVezesTestada)
     for (i = 0; i <= qntdVezesTestada + 1; i++)
     {
         j = 0;
-        while (j <= 4){
-            printf("\r %s%% %c", setPorcentagem(i,qntdVezesTestada, 100),loading(j));
+        while (j <= 4)
+        {
+            printf("\r %s%% %c", setPorcentagem(i, qntdVezesTestada, 100), loading(j));
             fflush(stdout);
-            for (int delay = 0; delay < 100000000; delay++);
+            for (int delay = 0; delay < 100000000; delay++)
+                ;
             j++;
-            
-        } 
-        if(i == qntdVezesTestada){
+        }
+        if (i == qntdVezesTestada)
+        {
             break;
         }
         start_time = clock();
@@ -61,9 +61,6 @@ double *complexidadeMedia(void(func)(int *, int), int qntdVezesTestada)
         double tempoPassado = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
         tempos[i] = tempoPassado;
         memcpy(array, valores, tamanho * sizeof(int));
-        
-        
-        
     }
     free(array);
     return tempos;
@@ -97,16 +94,17 @@ char loading(int i)
         return '\\';
     else if (i == 2)
         return '|';
-    else if(i == 3)
+    else if (i == 3)
         return '/';
-    else 
-    return '-';
+    else
+        return '-';
 }
 
-char *setPorcentagem(int qntdTestes, int total, int tamanhoBarra){
-     int porcentagem = (int)(((double)qntdTestes / total) * 100);
+char *setPorcentagem(int qntdTestes, int total, int tamanhoBarra)
+{
+    int porcentagem = (int)(((double)qntdTestes / total) * 100);
 
-    int numPreenchidos = (porcentagem * tamanhoBarra + 50) / 100;
+    int numPreenchidos = (porcentagem * tamanhoBarra) / 100;
 
     char *barraString = (char *)malloc((tamanhoBarra + 6) * sizeof(char));
     barraString[0] = '[';
@@ -115,7 +113,7 @@ char *setPorcentagem(int qntdTestes, int total, int tamanhoBarra){
         barraString[i] = (i <= numPreenchidos) ? '=' : ' ';
     }
     char aux[4];
-    sprintf(aux,"%i", porcentagem);
+    sprintf(aux, "%i", porcentagem);
     barraString[tamanhoBarra + 1] = ']';
     barraString[tamanhoBarra + 2] = ' ';
     barraString[tamanhoBarra + 3] = aux[0];
@@ -123,5 +121,23 @@ char *setPorcentagem(int qntdTestes, int total, int tamanhoBarra){
     barraString[tamanhoBarra + 5] = aux[2];
     barraString[tamanhoBarra + 6] = '\0';
     return barraString;
+}
 
+void escreverEmArquivo(double *tempo, int qntdDeTestes)
+{
+
+    char *nomeDoArquivo;
+    srand(time(NULL));
+    sprintf(nomeDoArquivo, "Tempos%i.txt", rand() % 100);
+    FILE *arquivoDeTempo = fopen(nomeDoArquivo, "w");
+    if (arquivoDeTempo == NULL)
+    {
+        printf("\nNão foi possivel abrir ou criar o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < qntdDeTestes; i++)
+    {
+        fprintf(arquivoDeTempo, "Tempo [%i] = %f", i + 1, tempo);
+    }
 }
