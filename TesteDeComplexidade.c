730a0e75b1303326db505af3588844e8ecee8d26
Bazch <mikael.vidal@gmail.com>
2024-06-19 14:29:49 -0300

    Funções polinomiais implementadas

--- TesteDeComplexidade.c
+++ TesteDeComplexidade.c
@@ -3,15 +3,20 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
-double *complexidadeMedia(void(func)(int *, int, int), int qntdVezesTestada, long int tamanho, int seed);
+double *complexidadeMedia(void(func)(int *, int, int), int qntdOrdenacoes, int tamanho, int seed);
+void verificarAlocacaoMatriz(double **ptrr, char *local);
+void verificarAlocacaoPonteiroDouble(double *ptr, char *local);
+void verificarAlocacaoPonteiroint(int *ptr, char *local);
 void insertionSort(int *arr, int n);
 void merge(int arr[], int l, int m, int r);
 void merge_sort(int arr[], int l, int r);
 char loading(int i);
+void geradorRandom(int *array, int seed, int qntd);
 char *porcentagemBarra(int qntdTestes, int total, int tamanhoBarra);
-void escreverEmArquivo(double *tempo, int qntdDeTestes, long int n, int i);
-double **MMQPrimeiroGrau(double *temposMedios, long int *tamanhosN, int testes);
-double **MMQSegundoGrau(double *temposMedios, long int *tamanhosN, int testes);
+void escreverEmArquivo(double *tempo, int qntdDeTestes, int n, int i);
+double **MMQPrimeiroGrau(double *temposMedios, int *tamanhosN, int testes);
+double **MMQSegundoGrau(double *temposMedios, int *tamanhosN, int testes);
+double **MMQTerceiroGrau(double *temposMedios, int *tamanhosN, int testes);
 char *extrairDoisPrimeirosDigitos(double numero, int qntExtracao);
 void temposMediosGet(double *temposMedios, int qntTamanhos);
 double **calcularTransposta(double **A, int linhas, int colunas);
@@ -23,54 +28,62 @@ int ehInversivel(double **matriz, int n);
 void imprimirMat(double **matriz, int lin, int col, int qntDigitos);
 int main()
 {
-    int qntdTestes = 100;
-    long int tamanho = 1000;
-    int testes = 10;
+    int qntdOrdenacoes = 20;
+    int tamanho = 10000;
+    int testes = 50;
     double *tempos;
-    long int *tamanhos = (long int *)malloc(10 * sizeof(long int));
-    double *temposMediosVet = (double *)malloc(10 * sizeof(double));
-    for (int i = 0; i < testes; tamanho *= 2, i++)
+    int *tamanhos = (int *)malloc(testes * sizeof(int));
+    double *temposMediosVet = (double *)malloc(testes * sizeof(double));
+    verificarAlocacaoPonteiroint(tamanhos, "ponteiro de tamanhos");
+    verificarAlocacaoPonteiroDouble(temposMediosVet, "Ponteiro com tempos medios");
+    for (int i = 0; i < 20; tamanho += 10000, i++)
     {
         tamanhos[i] = tamanho;
-        /* tempos = complexidadeMedia(merge_sort, qntdTestes, tamanho, time(NULL));
-        escreverEmArquivo(tempos, qntdTestes, tamanho, i); */
+         /* tempos = complexidadeMedia(merge_sort, qntdOrdenacoes, tamanho, time(NULL));
+        escreverEmArquivo(tempos, qntdOrdenacoes, tamanho, i);  */
     }
-    temposMediosGet(temposMediosVet, qntdTestes);
-    MMQSegundoGrau(temposMediosVet, tamanhos, testes);
+
+    temposMediosGet(temposMediosVet, testes);
+    MMQTerceiroGrau(temposMediosVet, tamanhos, testes);
     free(tempos);
 }
-
-int *geradorRandom(int seed, int qntd)
+void geradorRandom(int *array, int seed, int qntd)
 {
     int i = 0;
     srand(seed);
-    int *vetor = (int *)malloc(qntd * sizeof(int));
+    
+
+    verificarAlocacaoPonteiroint(array, "vetor no gerador random com numeros gerados aleatoriamente");
+
     for (i = 0; i < qntd; i++)
     {
-        vetor[i] = rand() % qntd;
+        array[i] = rand() % qntd;
     }
-    return vetor;
 }
 
 // Essa função deve retornar um vetor de tempos do algoritmo que está sendo passado
-double *complexidadeMedia(void(func)(int *, int, int), int qntdVezesTestada, long int tamanho, int seed)
+double *complexidadeMedia(void(func)(int *, int, int), int qntdOrdenacoes, int tamanho, int seed)
 {
     clock_t start_time, end_time;
     int i, j;
     // variavel que vai armazenar os tempos de cada ordenação de um determinado vetor
-    double *tempos = (double *)malloc(qntdVezesTestada * sizeof(double));
+    double *tempos = (double *)malloc(qntdOrdenacoes * sizeof(double));
+    verificarAlocacaoPonteiroDouble(tempos, "ponteiro com os tempos na função complexidade media");
     // Função que recebe uma seed qualquer e o tamanho do vetor, e vai gerar uma quantidade de valores para preencher o vetor igual ao tamanho
-    int *array = geradorRandom(seed, tamanho);
-    // Neste laço realizamos os testes com o algoritmo de ordenação em questão uma quantidade de vezes = qntdVezesTestada, para verificar e chegarmos a qual é o tempo mais proximo
+    int *array = (int *)malloc(tamanho * sizeof(int));
+    verificarAlocacaoPonteiroint(array, "vetor com os numeros aleatorios na função complexidade media");
+    geradorRandom(array, seed, tamanho);
+
+    // Neste laço realizamos os testes com o algoritmo de ordenação em questão uma quantidade de vezes = qntdOrdenacoes, para verificar e chegarmos a qual é o tempo mais proximo
     // Que o algoritmo em analise leva para ordenar essa quantidade definida de valores, quanto mais testes, mais preciso o calculo de tempo
-    for (i = 0; i <= qntdVezesTestada + 1; i++)
+    for (i = 0; i <= qntdOrdenacoes + 1; i++)
     {
 
         j = 0;
         // Este laço serve para mostrar uma barra de progresso na conclusão dos testes com base na quantidade de vezes testada
         while (j <= 4)
         {
-            printf("\r %s%% %c", porcentagemBarra(i, qntdVezesTestada, 100), loading(j));
+            printf("\r %s% %%c", porcentagemBarra(i, qntdOrdenacoes, 100), loading(j));
             fflush(stdout);
             for (int delay = 0; delay < 100000000; delay++)
                 ;
@@ -78,7 +91,7 @@ double *complexidadeMedia(void(func)(int *, int, int), int qntdVezesTestada, lon
         }
 
         // Uma pequena condição para que a barra anterior mostre o progresso quando chegar em 100%
-        if (i == qntdVezesTestada)
+        if (i == qntdOrdenacoes)
         {
             break;
         }
@@ -91,7 +104,7 @@ double *complexidadeMedia(void(func)(int *, int, int), int qntdVezesTestada, lon
         double tempoPassado = ((double)(end_time - start_time)) / CLOCKS_PER_SEC * 1000.0;
         tempos[i] = tempoPassado;
         seed = rand() % tamanho;
-        array = geradorRandom(seed, tamanho);
+        geradorRandom(array ,seed, tamanho);
     }
     free(array);
     return tempos;
@@ -217,7 +230,7 @@ char *porcentagemBarra(int qntdTestes, int total, int tamanhoBarra)
     return barraString;
 }
 // Escreve os tempos em um novo arquivo com nome gerado aleatoriamente
-void escreverEmArquivo(double *tempo, int qntdDeTestes, long int n, int i)
+void escreverEmArquivo(double *tempo, int qntTempos, int n, int i)
 {
     double tempoMedio = 0;
     char *nomeDoArquivo = (char *)malloc((20) * sizeof(char));
@@ -233,17 +246,18 @@ void escreverEmArquivo(double *tempo, int qntdDeTestes, long int n, int i)
 
     fprintf(arquivoDeTempo, "\nTamanho do problema \t Tempo\n");
 
-    for (int i = 0; i < qntdDeTestes; i++)
+    for (int i = 0; i < qntTempos; i++)
     {
         fprintf(arquivoDeTempo, "\n\t\t%li   \t\t   %f", n, tempo[i]);
         tempoMedio += tempo[i];
     }
 
-    tempoMedio /= qntdDeTestes;
+    tempoMedio /= qntTempos;
 
     fprintf(arquivoDeTempo, "\nTempo medio do algoritmo: %f", tempoMedio);
     fclose(arquivoDeTempo);
     free(nomeDoArquivo);
+    free(arquivoDeTempo);
 }
 // Função que vai armazenar os tempos medios dos arquivos gerados anteriormente em um array de tempos medios
 void temposMediosGet(double *temposMedios, int qntTamanhos)
@@ -270,14 +284,18 @@ void temposMediosGet(double *temposMedios, int qntTamanhos)
 }
 
 // Função a qual será usada para realizar o calculo do MMQ de uma matriz
-double **MMQPrimeiroGrau(double *temposMedios, long int *tamanhosN, int testes)
+double **MMQPrimeiroGrau(double *temposMedios, int *tamanhosN, int testes)
 {
     double **A = (double **)malloc(testes * sizeof(double *));
     double **y = (double **)malloc(testes * sizeof(double *));
+    verificarAlocacaoMatriz(A, "Matriz A MMQ");
+    verificarAlocacaoMatriz(y, "Matriz y MMQ");
     for (int i = 0; i < testes; i++)
     {
         A[i] = (double *)malloc(2 * sizeof(double));
         y[i] = (double *)malloc(2 * sizeof(double));
+        verificarAlocacaoPonteiroDouble(A[i], "colunas da matriz A");
+        verificarAlocacaoPonteiroDouble(y[i], "colunas da matriz y");
         A[i][0] = 1;
         A[i][1] = tamanhosN[i];
         y[i][0] = 1;
@@ -344,14 +362,18 @@ double **MMQPrimeiroGrau(double *temposMedios, long int *tamanhosN, int testes)
     free(y);
 }
 
-double **MMQSegundoGrau(double *temposMedios, long int *tamanhosN, int testes)
+double **MMQSegundoGrau(double *temposMedios, int *tamanhosN, int testes)
 {
     double **A = (double **)malloc(testes * sizeof(double *));
     double **y = (double **)malloc(testes * sizeof(double *));
+    verificarAlocacaoMatriz(A, "Matriz A MMQ");
+    verificarAlocacaoMatriz(y, "Matriz y MMQ");
     for (int i = 0; i < testes; i++)
     {
         A[i] = (double *)malloc(3 * sizeof(double));
         y[i] = (double *)malloc(3 * sizeof(double));
+        verificarAlocacaoPonteiroDouble(A[i], "colunas da matriz A");
+        verificarAlocacaoPonteiroDouble(y[i], "colunas da matriz y");
         A[i][0] = 1;
         A[i][1] = tamanhosN[i];
         A[i][2] = pow(tamanhosN[i], 2);
@@ -362,20 +384,20 @@ double **MMQSegundoGrau(double *temposMedios, long int *tamanhosN, int testes)
     // Realizando calculos necessarios para resolver o sistema A, B e C
     printf("\n\n");
     printf("Matriz A: \n");
-    imprimirMat(A, 10, 3, 7);
+    imprimirMat(A, 10, 3, 5);
     printf("\n");
     printf("Matriz Y: \n");
 
-    imprimirMat(y, 10, 2, 6);
+    imprimirMat(y, 10, 3, 6);
     printf("\n");
 
     double **Atransposta = calcularTransposta(A, testes, 3);
     printf("Matriz A transposta: \n");
-    imprimirMat(Atransposta, 3, 10, 7);
+    imprimirMat(Atransposta, 3, 10, 6);
     printf("\n");
     double **AtA = produtoEntreMatrizes(A, Atransposta, testes, 3, testes);
     printf("Matriz A * A transposta: \n");
-    imprimirMat(AtA, testes, testes, 10);
+    imprimirMat(AtA, testes, testes, 6);
     printf("\n");
     if (ehInversivel(AtA, 3) == 1)
     {
@@ -424,9 +446,11 @@ double **MMQSegundoGrau(double *temposMedios, long int *tamanhosN, int testes)
 double **calcularTransposta(double **A, int linhas, int colunas)
 {
     double **transposta = (double **)malloc(colunas * sizeof(double *));
+    verificarAlocacaoMatriz(transposta, "Matriz transposta");
     for (int i = 0; i < colunas; i++)
     {
         transposta[i] = (double *)malloc(linhas * sizeof(double));
+        verificarAlocacaoPonteiroDouble(transposta[i], "colunas da matriz transposta");
     }
 
     for (int i = 0; i < linhas; i++)
@@ -443,8 +467,15 @@ double **calcularTransposta(double **A, int linhas, int colunas)
 double **produtoEntreMatrizes(double **A, double **B, int linhasA, int colunasA, int colunasB)
 {
     double **result = (double **)malloc(linhasA * sizeof(double *));
+    verificarAlocacaoMatriz(result, "Matriz result produto entre matrizes");
+
     for (int i = 0; i < linhasA; i++)
+    {
+
         result[i] = (double *)malloc(colunasB * sizeof(double));
+        verificarAlocacaoPonteiroDouble(result[i], "colunas da matriz result (produto entre matrizes)");
+    }
+
     for (int i = 0; i < linhasA; i++)
         for (int j = 0; j < colunasB; j++)
         {
@@ -459,10 +490,14 @@ double **calcularInversa(double **matriz, int n)
 {
     double **inversa = (double **)malloc(n * sizeof(double *));
     double **temp = (double **)malloc(n * sizeof(double *));
+    verificarAlocacaoMatriz(inversa, "Matriz inversa");
+    verificarAlocacaoMatriz(temp, "Matriz temp na inversa");
     for (int i = 0; i < n; i++)
     {
         inversa[i] = (double *)malloc(n * sizeof(double));
         temp[i] = (double *)malloc(n * sizeof(double));
+        verificarAlocacaoPonteiroDouble(inversa[i], "colunas da matriz inversa");
+        verificarAlocacaoPonteiroDouble(temp[i], "colunas da matriz temp na inversa");
         for (int j = 0; j < n; j++)
         {
             if (i == j)
@@ -522,9 +557,12 @@ double determinant(double **matriz, int n)
 
     // Alocando memória para a matriz temporária
     double **temp = (double **)malloc(n * sizeof(double *));
+    verificarAlocacaoMatriz(temp, "Matriz temp no calculo do determinante");
+
     for (int i = 0; i < n; i++)
     {
         temp[i] = (double *)malloc(n * sizeof(double));
+        verificarAlocacaoPonteiroDouble(temp[i], "colunas da matriz temp no calculo do determinante");
     }
 
     int sign = 1;
@@ -574,11 +612,10 @@ void getCofactor(double **matriz, double **temp, int p, int q, int n)
         }
     }
 }
-
 int ehInversivel(double **matriz, int n)
 {
     double det = determinant(matriz, n);
-    // printf("\n%f\n", det);
+    //  printf("\n%f\n", det);
     if (det != 0)
     {
         return 1;
@@ -623,4 +660,114 @@ char *extrairDoisPrimeirosDigitos(double numero, int qntExtracao)
     extrair[qntExtracao - 1] = '\0';
 
     return extrair;
+}
+
+void verificarAlocacaoPonteiroint(int *ptr, char *local)
+{
+    if (ptr == NULL)
+    {
+        printf("\nFalha na alocação de memoria %s", local);
+        exit(EXIT_FAILURE);
+    }
+}
+void verificarAlocacaoPonteiroDouble(double *ptr, char *local)
+{
+    if (ptr == NULL)
+    {
+        printf("\nFalha na alocação de memoria %s", local);
+        exit(EXIT_FAILURE);
+    }
+}
+void verificarAlocacaoMatriz(double **ptrr, char *local)
+{
+    if (ptrr == NULL)
+    {
+        printf("\nFalha na alocação de memoria %s", local);
+        exit(EXIT_FAILURE);
+    }
+}
+
+double **MMQTerceiroGrau(double *temposMedios, int *tamanhosN, int testes){
+    double **A = (double **)malloc(testes * sizeof(double *));
+    double **y = (double **)malloc(testes * sizeof(double *));
+    verificarAlocacaoMatriz(A, "Matriz A MMQ");
+    verificarAlocacaoMatriz(y, "Matriz y MMQ");
+    for (int i = 0; i < testes; i++)
+    {
+        A[i] = (double *)malloc(4 * sizeof(double));
+        y[i] = (double *)malloc(4 * sizeof(double));
+        verificarAlocacaoPonteiroDouble(A[i], "colunas da matriz A");
+        verificarAlocacaoPonteiroDouble(y[i], "colunas da matriz y");
+        A[i][0] = 1;
+        A[i][1] = tamanhosN[i];
+        A[i][2] = pow(tamanhosN[i], 2);
+        A[i][3] = pow(tamanhosN[i], 3);
+        y[i][0] = 1;
+        y[i][1] = temposMedios[i];
+        y[i][2] = pow(temposMedios[i], 2);
+        y[i][3] = pow(temposMedios[i], 3);
+
+    }
+    // Realizando calculos necessarios para resolver o sistema A, B e C
+    printf("\n\n");
+    printf("Matriz A: \n");
+    imprimirMat(A, 10, 4, 5);
+    printf("\n");
+    printf("Matriz Y: \n");
+
+    imprimirMat(y, 10, 4, 6);
+    printf("\n");
+
+    double **Atransposta = calcularTransposta(A, testes, 4);
+    printf("Matriz A transposta: \n");
+    imprimirMat(Atransposta, 4, 10, 6);
+    printf("\n");
+    double **AtA = produtoEntreMatrizes(A, Atransposta, testes, 4, testes);
+    printf("Matriz A * A transposta: \n");
+    imprimirMat(AtA, testes, testes, 6);
+    printf("\n");
+    if (ehInversivel(AtA, 4) == 1)
+    {
+
+        double **AtY = produtoEntreMatrizes(Atransposta, y, 4, testes, 4);
+        printf("Matriz A tranposta*y: \n");
+        imprimirMat(AtY, 4, 4, 5);
+        printf("\n");
+        double **AtA_inv = calcularInversa(AtA, 4);
+        printf("Matriz inversa de: A * A transposta: \n");
+        imprimirMat(AtA_inv, 4, 4, 5);
+        printf("\n");
+        double **result = produtoEntreMatrizes(AtA_inv, AtY, 4, 4, 1);
+        printf("\n");
+        printf("a = %s\n", extrairDoisPrimeirosDigitos(result[0][0], 4));
+        printf("b = %s\n", extrairDoisPrimeirosDigitos(result[1][0], 4));
+        printf("c = %s\n", extrairDoisPrimeirosDigitos(result[2][0], 4));
+        printf("d = %s\n", extrairDoisPrimeirosDigitos(result[3][0], 4));
+
+        for (int i = 0; i < 4; i++)
+        {
+            free(Atransposta[i]);
+            free(AtA[i]);
+            free(AtY[i]);
+            free(AtA_inv[i]);
+            free(result[i]);
+        }
+        free(Atransposta);
+        free(AtA);
+        free(AtY);
+        free(AtA_inv);
+        free(result);
+    }
+    else
+    {
+        printf("\nA matriz gerada não é inversivel\n");
+    }
+
+    for (int i = 0; i < testes; i++)
+    {
+        free(A[i]);
+        free(y[i]);
+    }
+    free(A);
+    free(y);
 }
