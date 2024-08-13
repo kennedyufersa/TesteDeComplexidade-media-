7bfd8a46301d2a0eb28f044ba1d804b109b1e261
Bazch <mikael.vidal@gmail.com>
2024-08-13 08:33:15 -0300

    att para metodo do gradiente descendente

--- MetodoGradiente.c
+++ MetodoGradiente.c
@@ -1,29 +1,30 @@
 #include "MetodoGradiente.h"
 
-double *funcaoErroQuadratico(double *coeficientes, double *tempos, double *tamanhos, int iteracoes, int qntPontos, double taxaDeAprendizado)
+double *funcaoErroQuadratico(double *coeficientes, double *y, double *x, int qntPontos, double taxaDeAprendizado)
 {
     int i, j;
     double erro = 2;
     double a_db, b_db;
+
     while (erro > 1)
     {
         erro = 0;
 
         for (int i = 0; i < qntPontos; i++)
         {
-            erro += pow((tempos[i] - coeficientes[1] - coeficientes[0] * tamanhos[i]), 2);
+            erro += pow((y[i] - coeficientes[1] - coeficientes[0] * x[i]), 2);
         }
 
         a_db = b_db = 0.0;
 
         for (i = 0; i < qntPontos; i++)
         {
-            a_db += (tempos[i] - coeficientes[1] - coeficientes[0]) * (-tamanhos[i]);
-            b_db += (tempos[i] - coeficientes[1] - coeficientes[0]) * (-1);
+            a_db += (y[i] - coeficientes[1] - coeficientes[0]) * (-x[i]);
+            b_db += (y[i] - coeficientes[1] - coeficientes[0]) * (-1);
         }
 
-        coeficientes[0] -= taxaDeAprendizado * (a_db/qntPontos);
-        coeficientes[1] -= taxaDeAprendizado * (b_db/qntPontos);
+        coeficientes[0] -= taxaDeAprendizado * a_db;
+        coeficientes[1] -= taxaDeAprendizado * b_db;
 
         printf("a = %f, b = %f, erro = %f\n", coeficientes[0], coeficientes[1], erro);
     }
--- main.c
+++ main.c
@@ -3,45 +3,31 @@
 #include "VerificacaoDeAlocacao.c"
 #include "AlgoritmosDeOrdenacao.c"
 #include "MetodoGradiente.c"
+#include "FuncoesMMQ.c"
 int main()
 {
     int qntdOrdenacoes = 100;
-    int tamanho = 100;
-    int testes = 88;
+    int tamanho = 1000;
+    int testes = 100;
     double *tempos;
     double *tamanhos = (double *)malloc(testes * sizeof(double));
     double *temposMediosVet = (double *)malloc(testes * sizeof(double));
-    double *coeficientes = (double *)malloc(2 * sizeof(double));
-    coeficientes[0] = 50;
-    coeficientes[1] = 20;
+    double *coeficientes_e = (double *)malloc(2 * sizeof(double));
+
     verificarAlocacaoPonteiroint(tamanhos, "ponteiro de tamanhos");
     verificarAlocacaoPonteiroDouble(temposMediosVet, "Ponteiro com tempos medios");
-    for (int i = 0; i < testes; tamanho += 100, i++)
+    for (int i = 0; i < testes; tamanho += 1000, i++)
     {
         tamanhos[i] = tamanho;
-        /*   tempos = complexidadeMedia(merge_sort, qntdOrdenacoes, tamanho, time(NULL));
-       escreverEmArquivo(tempos, qntdOrdenacoes, tamanho, i);   */
+        /* tempos = complexidadeMedia(merge_sort, qntdOrdenacoes, tamanho, time(NULL));
+        escreverEmArquivo(tempos, qntdOrdenacoes, tamanho, i); */
     }
 
     temposMediosGet(temposMediosVet, testes);
-  /*   double max_tamanho = tamanhos[0];
-    for (int i = 1; i < testes; i++)
-    {
-        if (tamanhos[i] > max_tamanho)
-        {
-            max_tamanho = tamanhos[i];
-        }
-    }
+    coeficientes_e[0] = (temposMediosVet[testes-1] - temposMediosVet[0]) / (tamanhos[testes - 1] - tamanhos[0]);
+    coeficientes_e[1] = 0;
 
-    if (max_tamanho != 0) // Garantir que não há divisão por zero
-    {
-        for (int i = 0; i < testes; i++)
-        {
-            tamanhos[i] = tamanhos[i] / max_tamanho; // Normalização
-        }
-    } */
-
-    coeficientes = funcaoErroQuadratico(coeficientes, temposMediosVet, tamanhos, 1000, testes, 0.0000000001);
-    plotGraphGNU(temposMediosVet, tamanhos, testes, coeficientes[0], coeficientes[1], 1);
-    // free(tempos);
+    coeficientes_e = funcaoErroQuadratico(coeficientes_e, temposMediosVet, tamanhos, testes, 0.00000001);
+    plotGraphGNU(temposMediosVet, tamanhos, testes, coeficientes_e[0], coeficientes_e[1], 1);
+    free(tempos);
 }
